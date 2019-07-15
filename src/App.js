8be26440fe3d3027784892d76751880b3a6950e6
Bruno <brunopjsw@gmail.com>
2019-07-15 06:25:49 -0300

    redux

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import GlobalStyles from './styles/global'
 import { Provider } from 'react-redux';
 import Routes from './routes';
 import Header from './components/Header'
-import store from './store'
+import './config/reactotronConfig'
+import store from './store';
 
 function App() {
   return (
